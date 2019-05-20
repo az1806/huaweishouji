33cb5319e445b6e8de3ea500a516ac6cc513a9e0
ANlipu <1990444768@qq.com>
2019-05-20 23:56:17 +0800

    ““李普”

---
+++ src/com/daolmpl/ShangPinDaoimpl.java
@@ -0,0 +1,46 @@
+package com.daolmpl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import com.dao.ShangPinDao;
+import com.entitly.ShangPin;
+
+
+import com.util.GongJuClass;
+
+
+
+public class ShangPinDaoimpl implements ShangPinDao{
+
+	
+
+	public List<ShangPin> queryShangPins() {
+		List<ShangPin> pts = new ArrayList<ShangPin>();
+		ResultSet rs = GongJuClass.querySQL("select * from splbdb;");
+		try {
+			while (rs.next()) {
+				ShangPin pt = new ShangPin();
+
+				pt.setId(rs.getInt(1));
+				pt.setName(rs.getString(2));
+				pts.add(pt);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return pts;
+	}
+
+
+
+	
+	}
+
+
